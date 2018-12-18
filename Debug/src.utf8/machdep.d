edd81d1f74106f3efcf5af9f47ac6ab64b6a4024
suzukiiichiro <suzuki.iichiro@gmail.com>
2018-12-18 12:49:09 +0900

    rm

--- Debug/src.utf8/machdep.d
+++
@@ -1,8 +0,0 @@
-src.utf8/machdep.o: ../src.utf8/machdep.c ../src.utf8/rogue.h \
- ../src.utf8/machdep.h ../src.utf8/init.h
-
-../src.utf8/rogue.h:
-
-../src.utf8/machdep.h:
-
-../src.utf8/init.h:
--- src.utf8/main.h
+++
@@ -1,8 +0,0 @@
-#if !defined( __MAIN_H__ )
-#define __MAIN_H__
-
-extern int read_mesg(char *argv_msgfile);
-extern void usage();
-
-#endif /* not __MAIN_H__ */
-//あ
